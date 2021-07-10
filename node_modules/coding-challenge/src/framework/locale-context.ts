1035f0877069a3039ed0c5b73481c3fbcb00df01
Steven Travers <steven.travers20@gmail.com>
2021-07-10 11:52:34 +0200

    remove node odules from git

--- node_modules/coding-challenge/src/framework/locale-context.ts
+++
@@ -1,6 +0,0 @@
-import React from "react"
-import { Locale } from "../models/locale"
-
-const defaultLocale = Locale.wrap('fr')
-
-export const LocaleContext = React.createContext(defaultLocale)
--- node_modules/coding-challenge/src/prelude/io-ts-types/newtype.ts
+++
@@ -1,4 +0,0 @@
-import * as t from 'io-ts'
-import { Iso } from 'monocle-ts'
-import { AnyNewtype, CarrierOf } from 'newtype-ts'
-export type NewTypeType<X extends AnyNewtype> = Iso<X, CarrierOf<X>> & t.Type<X, CarrierOf<X>>
